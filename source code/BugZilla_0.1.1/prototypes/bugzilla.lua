9d149c2eb8eb1ad3c2a33aca79a4e39afa22d47a
LaurensScheldeman <laurens.scheldeman@gmail.com>
2017-11-09 23:58:11 +0100

    BugZilla [0.1.1] Adding features
    - Fart-Cloud to counter act the belting bug
    - Multiple messages, so it doesn't get boring
    - Copies of biter images, ready to create custom sprites

---
+++ source code/BugZilla_0.1.1/prototypes/bugzilla.lua
@@ -0,0 +1,7 @@
+require ("prototypes/bugzilla-entities")
+require ("prototypes/bugzilla-builder")
+
+require ("prototypes/fart-cloud") -- Used by bugzilla-biter
+
+-- Creating bugs
+BZ_bugzilla.functions.make_alien(bugzilla_biter)

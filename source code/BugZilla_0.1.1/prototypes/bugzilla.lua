230995ff210b2b1bff770c01b34c203ee6104f1d
LaurensScheldeman <laurens.scheldeman@gmail.com>
2017-11-14 18:42:57 +0100

    Bugzilla [0.0.1] Initial MultiPlayer testing

--- source code/BugZilla_0.1.1/prototypes/bugzilla.lua
+++
@@ -1,7 +0,0 @@
-require ("prototypes/bugzilla-entities")
-require ("prototypes/bugzilla-builder")
-
-require ("prototypes/fart-cloud") -- Used by bugzilla-biter
-
--- Creating bugs
-BZ_bugzilla.functions.make_alien(bugzilla_biter)

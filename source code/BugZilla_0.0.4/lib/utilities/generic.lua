5f10f67d729dfa63bfade1d0378fd24e24ad9f5d
LaurensScheldeman <laurens.scheldeman@gmail.com>
2017-11-14 19:12:13 +0100

    Bugzilla [0.0.4] Increased volume on fart sound

---
+++ source code/BugZilla_0.0.4/lib/utilities/generic.lua
@@ -0,0 +1,24 @@
+require 'lib/utilities/math'
+
+function MessageAll(s)
+  game.print(s)
+
+  -- If mod ChatToFile is in the modlist, we can print this out too
+  if remote.interfaces.ChatToFile and remote.interfaces.ChatToFile.chat then
+    remote.call("ChatToFile", "chat", s)
+  end
+end
+
+function DeepCopy(original)
+  local original_type = type(original)
+  local copy
+  if original_type == 'table' then
+    copy = {}
+    for original_key, original_value in next, original, nil do
+      copy[DeepCopy(original_key)] = DeepCopy(original_value)
+    end
+  else -- number, string, boolean, etc
+    copy = original
+  end
+  return copy
+end

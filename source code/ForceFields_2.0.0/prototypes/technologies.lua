e19f9e1d1a74f342ec650da03646a33b22f99282
LaurensScheldeman <laurens.scheldeman@gmail.com>
2017-12-17 13:44:46 +0100

    ForceFields [2.0.0] Bugfix tech error

--- source code/ForceFields_2.0.0/prototypes/technologies.lua
+++ source code/ForceFields_2.0.0/prototypes/technologies.lua
@@ -105,7 +105,7 @@ data:extend(
         {
           {"science-pack-1", 1},
           {"science-pack-2", 1},
-          {"science-pack-3", 1}
+          {"science-pack-3", 1},
           {"military-science-pack", 1},
           {"high-tech-science-pack", 1}
         },

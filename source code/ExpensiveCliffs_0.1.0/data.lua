a17f5f76f3064bd214b6e3130d9479c0135e2448
LaurensScheldeman <laurens.scheldeman@gmail.com>
2018-01-10 01:16:40 +0100

    ExpensiveCliffs [0.1.0] Changed recipy

---
+++ source code/ExpensiveCliffs_0.1.0/data.lua
@@ -0,0 +1,10 @@
+
+data.raw.recipe["cliff-explosives"].energy_required = 60
+data.raw.recipe["cliff-explosives"].ingredients =
+  {
+    {"sulfuric-acid-barrel", 1},
+    {"explosives", 500},
+    {"iron-stick", 1},
+    {"red-wire", 100},
+    {"atomic-bomb", 1},
+  }

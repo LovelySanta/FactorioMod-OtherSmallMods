679fda45fc76fbcc0d884df93be72cad3a2c4a31
LaurensScheldeman <laurens.scheldeman@gmail.com>
2018-01-12 09:16:58 +0100

    ExpensiveCliffs [0.1.1] Start point

---
+++ source code/ExpensiveCliffs_0.1.1/control.lua
@@ -0,0 +1,20 @@
+
+-- on load game for first time
+script.on_init(function(_)
+  --log("on_init is running")
+
+  local map_gen_settings = game.surfaces["nauvis"].map_gen_settings
+  local cliff_settings = map_gen_settings.cliff_settings
+
+  -- Frequency
+  -- Default 10, doubles each time for a lower setting
+  cliff_settings.cliff_elevation_interval = 5.0001144409179688 -- high
+
+  -- Size
+  -- Default 10, doubles each time for a lower setting
+  cliff_settings.cliff_elevation_0 = 2.5000572204589844 -- very big
+
+  map_gen_settings.cliff_settings = cliff_settings
+  game.surfaces["nauvis"].map_gen_settings = map_gen_settings
+
+end)

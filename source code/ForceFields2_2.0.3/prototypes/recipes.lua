812d9bd86804ee32b1dc7f666eb0a9b9f5ccdc4a
LaurensScheldeman <laurens.scheldeman@gmail.com>
2018-01-21 16:53:19 +0100

    ForceFields [2.0.3] Start point

---
+++ source code/ForceFields2_2.0.3/prototypes/recipes.lua
@@ -0,0 +1,18 @@
+data:extend(
+  {
+    {
+      type = "recipe",
+      name = "forcefield-emitter",
+      enabled = "false",
+      energy_required = 5,
+      ingredients =
+      {
+        {"steel-plate", 50},
+        {"battery", 200},
+        {"advanced-circuit", 20},
+        {"small-lamp", 4}
+      },
+      result = "forcefield-emitter"
+    }
+  }
+)

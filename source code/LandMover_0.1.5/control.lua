dc70c53a4ef0fc02aae335effccde7393b957901
LaurensScheldeman <laurens.scheldeman@gmail.com>
2018-01-22 23:30:08 +0100

    LandMover [0.1.5] Updated event handling
    New on_player_built_tile event in base game version 0.16.17

--- source code/LandMover_0.1.5/control.lua
+++ source code/LandMover_0.1.5/control.lua
@@ -1,13 +1,7 @@
 script.on_event(defines.events.on_player_built_tile, function(event)
-	local surface = game.surfaces[event.surface_index]
-	local pos = event.positions
-	local player = game.players[event.player_index]
-
-	-- Get amount of water tiles placed
-	local amount = #pos
-	local tile = surface.get_tile(pos[1].x,pos[1].y)
-	if (tile.valid and tile.name=="water") then
-		-- Give player same amount of landfill
-		player.insert{name="landfill", count=amount}
-	end
+  -- Check if the player used land mover
+  if event.item.name == "landmover" then
+    -- Give the player same amount of landfill als he used landmover
+    game.players[event.player_index].insert{name="landfill", count=#event.tiles}
+  end
 end)

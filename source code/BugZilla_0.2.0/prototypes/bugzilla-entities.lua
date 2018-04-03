162ae48295f3cbe7e28966aad076b515ec7069dd
LaurensScheldeman <laurens.scheldeman@gmail.com>
2018-04-03 23:49:16 +0200

    BugZilla [0.2.0] configured spitter zilla stats

--- source code/BugZilla_0.2.0/prototypes/bugzilla-entities.lua
+++ source code/BugZilla_0.2.0/prototypes/bugzilla-entities.lua
@@ -54,7 +54,7 @@ bugzilla_biter =
         scale = 5,
         tint1 = {r=0.3, g=0.9, b=0.3, a=0.75},
         --tint2 = {r=0.3, g=0.9, b=0.3, a=0.75},
-        health = 100000, --100000
+        health = 250000, --250000
         healing = 0.01,
         speed = 0.2,
         has_belt_immunity = true,
@@ -73,10 +73,10 @@ bugzilla_biter =
         volume = .8,
       },
       resistance = {
-        {type = "physical", decrease = 8, percent = 75},
-        {type = "fire", decrease = 25, percent = 75},
-        {type = "explosion", decrease = 20, percent = 75},
-        {type = "laser", decrease = 15, percent = 75},
+        {type = "physical", decrease = 10, percent = 85},
+        {type = "fire", decrease = 30, percent = 85},
+        {type = "explosion", decrease = 30, percent = 85},
+        {type = "laser", decrease = 20, percent = 85},
         {type = "toxic-gas", decrease = 100, percent = 100}
       },
       graphics = {

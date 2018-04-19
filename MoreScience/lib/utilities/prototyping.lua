39b0c0fd424d1cc8a096076ed12d677f1ba34ea2
LaurensScheldeman <laurens.scheldeman@gmail.com>
2018-04-19 21:48:43 +0200

    MoreScience [0.1.0] upgraded prototype utility

--- MoreScience/lib/utilities/prototyping.lua
+++ MoreScience/lib/utilities/prototyping.lua
@@ -46,6 +46,11 @@ function addRecipeUnlock(technologyName, recipeToAdd)
     if not data.raw["technology"][technologyName].effects then
       data.raw["technology"][technologyName].effects = {}
     end
+    for index, effect in pairs(data.raw["technology"][technologyName].effects) do
+      if effect.type == "unlock-recipe" and effect.recipe == recipeToAdd then
+        return
+      end
+    end
     table.insert(data.raw["technology"][technologyName].effects, {type = "unlock-recipe", recipe = recipeToAdd})
   end
 end
@@ -70,6 +75,13 @@ end
 
 function addScienceIngredient(technologyName, sciencePackAmount, sciencePackName)
   if data.raw["technology"][technologyName] and data.raw["technology"][technologyName].unit.ingredients then
+    for index, ingredient in pairs(data.raw["technology"][technologyName].unit.ingredients) do
+      if ingredient[1] == sciencePackName then
+        log("WARNING: Tried adding \'" .. sciencePackName .. "\' to \'" .. technologyName .. "\' which was already present. Increased amount instead.")
+        data.raw["technology"][technologyName].unit.ingredients[index][2] = ingredient[2] + sciencePackAmount
+        return
+      end
+    end
     table.insert(data.raw["technology"][technologyName].unit.ingredients, {sciencePackName, sciencePackAmount})
   end
 end

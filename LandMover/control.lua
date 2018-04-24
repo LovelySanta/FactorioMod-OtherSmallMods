93908618768fa651236e221ac72f7f537785b324
LaurensScheldeman <laurens.scheldeman@gmail.com>
2018-04-25 00:14:53 +0200

    LaldMover [0.1.7] Fixed recipes not unlocking on existing map

--- LandMover/control.lua
+++ LandMover/control.lua
@@ -1,3 +1,21 @@
+script.on_configuration_changed(function(data)
+  -- Check if this mod has changed
+  for modName, modData in pairs(data.mod_changes) do
+    if modName == "LandMover" then
+
+      -- Mod has beed added/changed make sure recipes are unlocked if it was researched
+      for _, force in pairs(game.forces) do
+        if force.technologies['landfill'] and force.technologies['landfill'].researched then
+          force.reset_technology_effects()
+        end
+      end
+
+      -- No need to look further, we found our mod already
+      break
+    end
+  end
+end)
+
 script.on_event(defines.events.on_player_built_tile, function(event)
   -- Check if the player used land mover
   if event.item.name == "landmover" then

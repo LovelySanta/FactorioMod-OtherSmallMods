1d0710355359db82094507ecfe40cf1bdf4f63e4
LovelySanta <laurens.scheldeman@gmail.com>
2018-05-31 13:05:33 +0200

    DeepStorage [0.1.0] Succesfully adding and removing multiple stacks in/out of a chest

--- DeepStorage/src/chestDatastructure.lua
+++ DeepStorage/src/chestDatastructure.lua
@@ -162,56 +162,141 @@ function ChestDatastructure:updateNextChest()
     local chestToUpdate = deepcopy(self.chestData["nextChest"])
     self.chestData["nextChest"] = deepcopy(self.chestData[chestToUpdate["surfaceIndex"]][chestToUpdate["surfacePosY"]][chestToUpdate["surfacePosX"]]["nextChest"])
 
-    -- Update chestData
-    local surfaceIndex = chestToUpdate["surfaceIndex"]
-    local surfacePosX  = chestToUpdate["surfacePosX"]
-    local surfacePosY  = chestToUpdate["surfacePosY"]
-    game.print("Updating chest on surface " .. surfaceIndex .. " located at (" .. surfacePosX .. "," .. surfacePosY .. ").")
+    -- Update chest
+    game.print("Updating chest on surface " .. chestToUpdate["surfaceIndex"] .. " located at (" .. chestToUpdate["surfacePosX"] .. "," .. chestToUpdate["surfacePosY"] .. ").")
 
-    local function getSingleItemInfo(inventory)
-      local contents = inventory.get_contents()
-      if tableIsEmpty(contents) then
-        return {["name"] = nil, ["count"] = 0, ["stacksize"] = 0}
-      end
+    self:updateChest(chestToUpdate["surfaceIndex"], chestToUpdate["surfacePosX"], chestToUpdate["surfacePosY"])
 
-      local contentsCount = -1
-      local contentsName = nil
-      for itemName, itemCount in pairs(contents) do
-        if itemCount > contentsCount then
-          contentsCount = itemCount
-          contentsName = itemName
-        end
-      end
-      log(game.item_prototypes[contentsName].stack_size)
-      return {["name"] = contentsName, ["count"] = contentsCount, ["stacksize"] = game.item_prototypes[contentsName].stack_size}
+    if self.chestData[chestToUpdate["surfaceIndex"]][chestToUpdate["surfacePosY"]][chestToUpdate["surfacePosX"]]["contents"] then
+      game.print("Now contains " .. self.chestData[chestToUpdate["surfaceIndex"]][chestToUpdate["surfacePosY"]][chestToUpdate["surfacePosX"]]["contents"].count ..
+        "x " .. self.chestData[chestToUpdate["surfaceIndex"]][chestToUpdate["surfacePosY"]][chestToUpdate["surfacePosX"]]["contents"].name)
     end
-    local chestData = self.chestData[surfaceIndex][surfacePosY][surfacePosX]
-    local chestInventory = chestData["inventoryEntity"].get_inventory(defines.inventory.car_trunk)
-    local chestContents = getSingleItemInfo(chestInventory)
-    local chestBehaviour = chestData["monitorEntity"].get_or_create_control_behavior()
+  end
+end
 
-    -- Check content
-    if not chestData["contents"] then
-      -- there was no previous content registered
-      if chestContents.name and chestContents.count > 0 then
-        -- now there is contents
-        chestData["contents"] = deepcopy(chestContents)
 
-        -- set the filters
+
+function ChestDatastructure:updateChest(surfaceIndex, surfacePosX, surfacePosY)
+
+  local function getSingleItemInfo(inventory)
+    local contents = inventory.get_contents()
+    if tableIsEmpty(contents) then
+      return {["name"] = nil, ["count"] = 0}
+    end
+
+    local contentsCount = -1
+    local contentsName = nil
+    for itemName, itemCount in pairs(contents) do
+      if itemCount > contentsCount then
+        if contentsName then
+          -- we found another item with a bigger amount, we need to delete the previous item
+          inventory.remove{name = contentsName, count = containsCount}
+        end
+        contentsCount = itemCount
+        contentsName = itemName
+      else
+        -- we found another item with a lesser amount, we need to delete this item
+        inventory.remove{name = itemName, count = itemCount}
+      end
+    end
+
+    return {["name"] = contentsName, ["count"] = contentsCount}
+  end
+
+  local chestData = self.chestData[surfaceIndex][surfacePosY][surfacePosX]
+  local chestInventory = chestData["inventoryEntity"].get_inventory(defines.inventory.car_trunk)
+  local chestBehaviour = chestData["monitorEntity"].get_or_create_control_behavior()
+
+  local chestContents = getSingleItemInfo(chestInventory) --
+
+  -- Check content
+  if not chestData["contents"] then
+    -- there was no previous content registered
+    if chestContents.name and chestContents.count > 0 then
+      -- now there is contents
+      chestData["contents"] = deepcopy(chestContents)
+      chestData["contents"]["stacksize"] = game.item_prototypes[chestData["contents"].name].stack_size
+
+      -- set the filters
+      for inventoryIndex = 1, #chestInventory, 1 do
+        chestInventory.set_filter(inventoryIndex, chestContents.name)
+      end
+
+      -- if there is more items then 1 stack we need to remove them
+      if chestContents.count > chestData["contents"].stacksize then
+        chestInventory.remove{name = chestData.name, count = chestData["contents"].count - chestData["contents"].stacksize}
+      end
+
+      -- output signal on the monitor
+      chestBehaviour.parameters = {parameters = {{index = 1, signal = {type = "item", name = chestData["contents"].name}, count = chestData["contents"].count}}}
+    end
+
+  else -- there was content registered
+
+    if not chestContents.name then
+      if chestData["contents"].count <= chestData["contents"].stacksize then
+        -- chest is realy empty
+        chestData["contents"] = nil
+
+        -- remove the filters
         for inventoryIndex = 1, #chestInventory, 1 do
           chestInventory.set_filter(inventoryIndex, chestContents.name)
         end
 
-        -- output signal on the monitor
+        -- remove signal on the monitor
+        chestBehaviour.parameters = nil
+
+      else -- stack was fully outserted, but has still items in its buffer, time to fill it back up
+
+        -- update contents
+        chestData["contents"].count = chestData["contents"].count - chestData["contents"].stacksize
+
+        -- add items to the inventory
+        chestInventory.insert{name = chestData["contents"].name, count = min(chestData["contents"].count, chestData["contents"].stack_size)}
+
+        -- update the signal on the monitor
         chestBehaviour.parameters = {parameters = {{index = 1, signal = {type = "item", name = chestData["contents"].name}, count = chestData["contents"].count}}}
       end
+
     else
-      -- there was content registered
-      -- TODO
+      -- there was still some content in there
+      if chestContents.count > chestData["contents"].stacksize then -- more than a stack in the chest
+
+        -- update contents
+        chestData["contents"].count = chestData["contents"].count + (chestContents.count - chestData["contents"].stacksize)
+
+        -- remove excess items from the inventory
+        chestInventory.remove{name = chestData["contents"].name, count = chestContents.count - chestData["contents"].stacksize}
+
+      elseif chestContents.count < chestData["contents"].stacksize then -- less than a stack in the chest
+        if chestData["contents"].count >= chestData["contents"].stacksize then -- has more available items, insert more
+
+          -- update contents
+          chestData["contents"].count = chestData["contents"].count - (chestData["contents"].stacksize - chestContents.count)
+
+          -- add items to the invenory
+          chestInventory.insert{name = chestData["contents"].name, count = chestData["contents"].stacksize - chestContents.count}
+
+        else -- chest has less then a stack of content, nothing to insert
+
+          -- update contents
+          chestData["contents"].count = chestContents.count
+
+        end
+
+      else -- exactly a stack in the chest
+        if chestData["contents"].count == (chestData["contents"].stacksize - 1) then
+
+          -- update contents
+          chestData["contents"].count = chestContents.count
+
+        end
+      end
+
+      -- update the signal on the monitor
+      chestBehaviour.parameters = {parameters = {{index = 1, signal = {type = "item", name = chestData["contents"].name}, count = chestData["contents"].count}}}
+
     end
-
-
-
   end
 end
 

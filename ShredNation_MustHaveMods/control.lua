fff84cba1a4accfacd1eb7539d63650d4785a9da
LovelySanta <laurens.scheldeman@gmail.com>
2018-12-09 23:45:21 +0100

    added sfx

--- ShredNation_MustHaveMods/control.lua
+++ ShredNation_MustHaveMods/control.lua
@@ -3,8 +3,8 @@ local fart_name = {
   ["SN-sfx-2"] = "fart2.ogg",
   ["SN-sfx-3"] = "fart3.ogg",
   ["SN-sfx-4"] = "fart4.ogg",
-  ["SN-sfx-5"] = nil,
-  ["SN-sfx-6"] = nil,
+  ["SN-sfx-5"] = "fart5.ogg",
+  ["SN-sfx-6"] = "fart6.ogg",
   ["SN-sfx-7"] = nil,
   ["SN-sfx-8"] = nil,
   ["SN-sfx-9"] = nil,
@@ -19,7 +19,22 @@ local on_fart_key_pressed = function(event)
     sfx = fart_name["SN-sfx-"..global.generator(10-.000000001)]
   end
   local player = game.players[event.player_index]
-  if sfx and player and settings.get_player_settings(player)["SN-sfx-enabled"].value then
+  local can_play_sfx = function(player_index)
+    if not global.sfx_cooldown then global.sfx_cooldown = {} end
+    if not global.sfx_cooldown[player_index] then global.sfx_cooldown[player_index] = 0 end
+
+    if global.sfx_cooldown[player_index] + 60*30 >= game.tick then
+      if fart_name[event.input_name] ~= "random" then
+        game.players[player_index].print{"messages.sfx-cooldown"}
+      end
+      return false
+    else
+      global.sfx_cooldown[player_index] = game.tick
+      return true
+    end
+  end
+
+  if sfx and player and settings.get_player_settings(player)["SN-sfx-enabled"].value and can_play_sfx(event.player_index) then
     player.surface.play_sound{
       path     = sfx,
       position = player.position
--- ShredNation_MustHaveMods/prototypes/sfx.lua
+++ ShredNation_MustHaveMods/prototypes/sfx.lua
@@ -5,6 +5,8 @@ for _,sound in pairs{
   "fart2.ogg",
   "fart3.ogg",
   "fart4.ogg",
+  "fart5.ogg",
+  "fart6.ogg",
 } do
   data:extend{
       {

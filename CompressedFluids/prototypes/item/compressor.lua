a93c2048f2a94f2e74643b78f3775d4433b1c7aa
LovelySanta <laurens.scheldeman@gmail.com>
2019-02-26 18:42:40 +0100

    Updated CompressedFluids to 0.17

--- CompressedFluids/prototypes/item/compressor.lua
+++ CompressedFluids/prototypes/item/compressor.lua
@@ -4,7 +4,7 @@ data:extend{
     name = "fluid-compressor",
     icon = "__CompressedFluids__/graphics/icons/compressor.png",
     icon_size = 51,
-    flags = {"goes-to-quickbar"},
+    flags = data.raw["item"]["pump"].flags,
     subgroup = data.raw["item"]["pump"].subgroup,
     order = data.raw["item"]["pump"].order.."a[fluid-compressor]",
     place_result = "fluid-compressor",
